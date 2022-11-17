9877ef641b333ebc1dd6d6a74c81d462b83ee1b6
Junewoo Byeon <movebxeax@gmail.com>
2022-11-17 17:25:33 +0900

    [backend] util:: response manager overloading method added

--- backend/src/main/java/com/whereismyhome/util/ResponseManager.java
+++ backend/src/main/java/com/whereismyhome/util/ResponseManager.java
@@ -16,6 +16,18 @@ public class ResponseManager {
 		}
 	}
 	
+	protected ResponseEntity<?> createResponse(Object response)
+	{
+		if(response != null)
+		{
+			return ResponseEntity.ok().body(response);
+		}
+		else
+		{
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
 	protected ResponseEntity<?> createResponse(HttpStatus status)
 	{
 		return createResponse(status, null);
