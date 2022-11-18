b311dff594c754a633feb331f8fb283a8ce8cd90
pilseon <luster6951@naver.com>
2022-11-18 10:20:43 +0900

    [frontend] TradeView:: sidebar 버튼  수정

--- frontend/src/components/trade/TradeSideBar.vue
+++ frontend/src/components/trade/TradeSideBar.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="side-bar-wrapper">
     <template v-if="isShow">
-      <div class="side-bar" v-if="isShow"></div>
+      <div class="side-bar"></div>
     </template>
-    <v-btn color="primary" class="trade-side-bar-active-btn" @click="showSide">
-      {{ isShow ? "닫기" : "열기" }}
-    </v-btn>
+    <button id="trade-side-bar-active-btn" @click="showSide">
+      <v-icon v-if="isShow">mdi-chevron-left</v-icon>
+      <v-icon v-else>mdi-chevron-right</v-icon>
+      <!-- {{ isShow ? "<" : ">" }} -->
+    </button>
   </div>
 </template>
 
@@ -28,8 +30,8 @@ export default {
 <style lang="scss" scoped>
 .side-bar-wrapper {
   background-color: yellow;
-  opacity: 90%;
   .side-bar {
+    opacity: 90%;
     width: 500px;
     // position: relative;
     // height: 100%;
@@ -38,16 +40,20 @@ export default {
   }
 }
 
-.trade-side-bar-active-btn {
+#trade-side-bar-active-btn {
   position: absolute;
   top: 350px;
-  right: -65px;
-  width: 30px;
-  height: 30px;
+  padding: 0 0 0 0;
+  background-color: white;
+  width: 25px;
+  height: 40px;
+  right: -25px;
+  // height: 30px;
   // border: 1px solid aliceblue;
-  box-sizing: border-box;
-  font-weight: 600;
-  font-size: 20px;
-  color: black;
+  border-bottom: 1px solid grey;
+  border-top: 1px solid grey;
+  border-right: 1px solid grey;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
 }
 </style>
