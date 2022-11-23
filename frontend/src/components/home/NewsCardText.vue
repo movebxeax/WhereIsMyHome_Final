18e6befa4852e1b4c7c86cb55a578953a8f1b880
Junewoo Byeon <movebxeax@gmail.com>
2022-11-24 00:43:34 +0900

    [frontend] News:: news implemented

--- frontend/src/components/home/NewsCardText.vue
+++ frontend/src/components/home/NewsCardText.vue
@@ -3,12 +3,12 @@
     <v-card-text class="pa-2 ma-1 text-left" v-for="(newsItem, index) in newsList" :key="index">
       <div class="d-flex">
         <div>
-          <v-btn text color="#3F72AF" @click="loadNoticeArticle(newsItem.no)">
-            {{ newsItem.subject }}
+          <v-btn text color="#3F72AF" @click="redirectTo(newsItem.link)">
+            {{ trimNewsTitle(newsItem.title) }}
           </v-btn>
         </div>
-        <div class="pr-4 ml-auto">
-          {{ newsItem.date }}
+        <div class="pr-4 pt-2 ml-auto">
+          <span style="mt-2">{{ toLocalDateString(newsItem.pubDate) }}</span>
         </div>
       </div>
     </v-card-text>
@@ -25,20 +25,59 @@
 
 <script>
 //import { httpNotice } from "@/utils/api.js";
+import axios from "axios";
+
 export default {
   name: "NewsCardText",
   data() {
     return {
-      newsList: {},
+      newsList: [],
     };
   },
   created() {
-    //httpNotice.get("").then(({ data }) => (this.noticeList = data));
+    axios.get("http://127.0.0.1:8080/api/news").then(({ data }) => (this.newsList = data.items));
   },
   methods: {
     // loadNoticeArticle(articleno) {
     //   this.$router.push(`/notice/detail/${articleno}`);
     // },
+    toLocalDateString(string) {
+      Date.prototype.YYYYMMDDHHMMSS = function () {
+        var yyyy = this.getFullYear().toString();
+        var MM = pad(this.getMonth() + 1, 2);
+        var dd = pad(this.getDate(), 2);
+        var hh = pad(this.getHours(), 2);
+        var mm = pad(this.getMinutes(), 2);
+        var ss = pad(this.getSeconds(), 2);
+
+        return yyyy + "-" + MM + "-" + dd + " " + hh + ":" + mm + ":" + ss;
+      };
+
+      function pad(number, length) {
+        var str = "" + number;
+        while (str.length < length) {
+          str = "0" + str;
+        }
+        return str;
+      }
+
+      let date = new Date(string);
+      return date.YYYYMMDDHHMMSS();
+    },
+    trimNewsTitle(string) {
+      function decodeHtml(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+      }
+
+      var htmlRegexG = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
+
+      return decodeHtml(string.replaceAll(htmlRegexG, "")).substr(0, 25) + "....";
+    },
+    redirectTo(link) {
+      window.open(link);
+    },
   },
 };
 </script>
