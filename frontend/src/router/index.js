f0e7e081dcbfaea1dbed7fb6df32d7e2107e747b
pilseon <luster6951@naver.com>
2022-11-23 14:48:52 +0900

    [frontend] common:: 페이지 제목 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,12 +13,18 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {
+      title: "홈",
+    },
   },
   {
     path: "/trade",
     name: "trade",
     props: true,
     component: TradeView,
+    meta: {
+      title: "거래 정보",
+    },
   },
   {
     path: "/notice",
@@ -30,23 +36,35 @@ const routes = [
         path: "list",
         name: "noticeList",
         component: () => import("@/components/notice/NoticeList.vue"),
+        meta: {
+          title: "공지사항",
+        },
       },
       {
         path: "detail/:no",
         props: true,
         name: "noticedetail",
         component: () => import("@/components/notice/NoticeDetail.vue"),
+        meta: {
+          title: "공지사항 조회",
+        },
       },
       {
         path: "modify/:no",
         props: true,
         name: "noticemodify",
         component: () => import("@/components/notice/NoticeModify.vue"),
+        meta: {
+          title: "공지사항 수정",
+        },
       },
       {
         path: "register",
         name: "noticeregister",
         component: () => import("@/components/notice/NoticeRegister.vue"),
+        meta: {
+          title: "공지사항 작성",
+        },
       },
     ],
   },
@@ -54,29 +72,42 @@ const routes = [
     path: "/qna",
     name: "qna",
     component: QnaView,
+
     redirect: "/qna/list",
     children: [
       {
         path: "list",
         name: "qnalist",
         component: () => import("@/components/qna/QnaList.vue"),
+        meta: {
+          title: "Q&A",
+        },
       },
       {
         path: "detail/:no",
         props: true,
         name: "qnadetail",
         component: () => import("@/components/qna/QnaDetail.vue"),
+        meta: {
+          title: "Q&A 조회",
+        },
       },
       {
         path: "modify/:no",
         props: true,
         name: "qnamodify",
         component: () => import("@/components/qna/QnaModify.vue"),
+        meta: {
+          title: "Q&A 수정",
+        },
       },
       {
         path: "register",
         name: "qnaregister",
         component: () => import("@/components/qna/QnaRegister.vue"),
+        meta: {
+          title: "Q&A 읽기",
+        },
       },
     ],
   },
@@ -89,6 +120,9 @@ const routes = [
         path: "modify",
         name: "modify",
         component: () => import("@/components/user/UserInfoTab.vue"),
+        meta: {
+          title: "마이 페이지",
+        },
       },
       {
         path: "signout",
@@ -100,6 +134,9 @@ const routes = [
         name: "selected",
         props: (route) => ({ selected: String(route.params.selected) }),
         component: () => import("@/components/user/SignTab.vue"),
+        meta: {
+          title: "회원 가입 및 로그인",
+        },
       },
     ],
   },
@@ -109,6 +146,14 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+const makeTitle = (title) => (title ? `${title} | HappyHouse` : "HappyHouse");
+
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    document.title = makeTitle(to.meta.title);
+  });
 });
 
 export default router;
